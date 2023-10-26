fa8d82ddbf8aa8ee4ff10264ed96b047ac168d62
edlazo <101842174+edlazo@users.noreply.github.com>
2023-10-26 00:20:16 -0300

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -5,7 +5,7 @@ $dbusuario="root";      // aqui debes ingresar el nombre de usuario
                         // para acceder a la base
 $dbpassword=""; // password de acceso para el usuario de la
                          // linea anterior
-$db="personas";        // Seleccionamos la base con la cual trabajar
+$db="expo";        // Seleccionamos la base con la cual trabajar
 $conexion = new mysqli($dbhost, $dbusuario, $dbpassword,$db);//se crea un objeto $conexion con la conexión a la base de datos
 $conexion->set_charset("utf8");//especifica el conjunto de caracteres predeterminado que se utilizará al enviar datos hacia y desde el servidor de la base de datos.
 ?>
