c536bc7dc68962f6eaae6278db35850105f94439
edlazo <edlazo@colegiosolari.edu.ar>
2023-10-30 15:36:30 -0300

    Search companies with leters in buy realize

--- realizar_pago.php
+++ realizar_pago.php
@@ -1,76 +1,84 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="Realizar pago.css">
-	<title>Realizar Pago</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="Realizar pago.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+
+    <title>Realizar Pago</title>
     <link rel="shortcut icon" href="visa.ico" />
 </head>
+
 <body>
-	<br>
-	<br>
-	<h1>VISA</h1>
+    <script>
+        $(document).ready(function() {
+            // Inicializa Select2 en tu elemento select con el id "marca"
+            $('#marca').select2();
+        });
+    </script>
+    <br>
+    <br>
+    <h1>VISA</h1>
 
-	<div class="formulario">
-	            <form method="post" action="pago.php">
-            	<h2>REALIZAR PAGO</h2>
-                
-               	<label>Marca:</label>
-                <select name="marca" id="marca" required>
-                    <option value="null" selected disabled>Selecciona puesto</option>
-                    <?php
-                    include "conecta.php";
-                    if (!$conexion) 
-                    {
-                        echo "conexión fallida";
-                    }
-                    else
-                    {
-                        echo "conexion exitosa!";
-                        echo ' <br/>';
-                        echo"INSERCION DE REGISTROS"."<br/>";
+    <div class="formulario">
+        <form method="post" action="pago.php">
+            <h2>REALIZAR PAGO</h2>
 
-                        $sql = "SELECT nombre FROM marcas";
-                        $result = $conexion->query($sql);
-                        
-                        
-                        if ($result->num_rows > 0) {
-                            while ($row = $result->fetch_assoc()) {
-                                echo '<option value="' . $row['nombre'] . '">' . $row['nombre'] . '</option>';
-                            }
-                        } else {
-                            echo '<option value="">No se encontraron marcas</option>';
+            <label>Marca:</label>
+            <select name="marca" id="marca" required>
+                <option value="null" selected disabled>Selecciona puesto</option>
+                <?php
+                include "conecta.php";
+                if (!$conexion) {
+                    echo "conexión fallida";
+                } else {
+                    echo "conexion exitosa!";
+                    echo ' <br/>';
+                    echo "INSERCIÓN DE REGISTROS" . "<br/>";
+
+                    $sql = "SELECT nombre FROM marcas";
+                    $result = $conexion->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo '<option value="' . $row['nombre'] . '">' . $row['nombre'] . '</option>';
                         }
+                    } else {
+                        echo '<option value="">No se encontraron marcas</option>';
                     }
-                        // Cierra la conexión a la base de datos
-                        $conexion->close();
+                }
+                // Cierra la conexión a la base de datos
+                $conexion->close();
+                ?>
+            </select>
 
-                    ?>
-                </select>
-                    
-                   <br>    
+            <br>
 
 
- 
-                <div class="Dinero">
-                    <label class="espacio">Ingresar monto</label> 
-                    <input type="text" placeholder="Dinero a descontar" name="saldo" required>  
-                </div>   
-                
-                
-                
 
-                <div class="DNI">
-                	<label class="espacio">Ingresar DNI</label>
-                    <input type="text" placeholder="Dni" name="dnicon" required>        
-                </div>   
-               
-                <center><button center type="submit">CONFIRMAR PAGO</center>  
-                <br> 
-                <br> 
-                <br> 
-            </form>    
-	</div>
+            <div class="Dinero">
+                <label class="espacio">Ingresar monto</label>
+                <input type="text" placeholder="Dinero a descontar" name="saldo" required>
+            </div>
+
+
+
+
+            <div class="DNI">
+                <label class="espacio">Ingresar DNI</label>
+                <input type="text" placeholder="Dni" name="dnicon" required>
+            </div>
+
+            <center><button center type="submit">CONFIRMAR PAGO</center>
+            <br>
+            <br>
+            <br>
+        </form>
+    </div>
 </body>
+
 </html>
