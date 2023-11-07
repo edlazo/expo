730925fa589b43ed4839195816f9040ca6bbd7f0
edlazo <101842174+edlazo@users.noreply.github.com>
2023-11-07 15:14:06 -0300

    Update inserta.php

--- inserta.php
+++ inserta.php
@@ -1,25 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro realizado</title>
     <link rel="shortcut icon" href="visa.ico" />
 </head>
+
 <body>
     <style>
-        body{
+        body {
             background-color: #0F0745;
             color: white;
         }
-        h1{
+
+        h1 {
             text-align: center;
             font-style: italic;
             font-family: Arial, Helvetica, sans-serif;
         }
-        img{
+
+        img {
             width: 25%;
         }
+
         .contenedor {
             text-align: center;
             font-style: italic;
@@ -27,6 +32,7 @@
             font-size: x-large;
             font-weight: bold;
         }
+
         .buttons {
             text-align: center;
             display: flex;
@@ -35,7 +41,8 @@
         }
 
         a {
-            width: 15vw; /* Utiliza el ancho completo de la pantalla */
+            width: 15vw;
+            /* Utiliza el ancho completo de la pantalla */
             height: 50px;
             border: 1px solid white;
             border-radius: 30px;
@@ -47,61 +54,55 @@
             text-align: center;
             text-decoration: none;
             margin: 0 auto;
-            padding-top: 2%; /* Espaciado vertical entre botones */
+            padding-top: 2%;
+            /* Espaciado vertical entre botones */
             text-align: center;
         }
 
-        a:hover{
+        a:hover {
             background-color: white;
             color: #0F0745;
             border-color: #0F0745;
             transition: .5s;
         }
-        .nombre{
+
+        .nombre {
             color: red;
         }
-
-        
     </style>
     <h1>Registro realizado</h1>
     <div class="contenedor">
         <img src="verify.png" alt="verify">
     </div>
-    
-<div class="contenedor">
-<?php
-$dni = $_POST['dniins'];
-$nombre_apellido = $_POST['nombre_apellidoins'];
-$mail = $_POST['mailins'];
-include "conecta.php";
-if (!$conexion) 
-{
-    echo "conexión fallida";
-}
-else
-{
-echo "Inserción de registro exitoso"."<br/>"."<br/>";
-$ingreso = "INSERT INTO `personas` (`id`, `nombre_apellido`, `mail`, `dni`) VALUES (NULL, '$nombre_apellido', '$mail', '$dni')";//
-if ($conexion->query($ingreso)) 
-{
-    echo "Bienvenido <span class='nombre'>" . $nombre_apellido . "</span> a la Expo Solari 2023<br>";
-    echo "Tiene un crédito de <span class='nombre'>\$100000</span> disponible para las compras";
-} else {
-    echo "Error: " . $ingreso . "<br>" . $conexion->error;
-}
-}
-$conexion->close(); //CIERRA LA CONEXION CON LA BASE
-?>
-  
-<br>
-<br>
-<div class="buttons">
-    <a href="index.html">Volver al inicio</a>
-</div>
 
-</div>
+    <div class="contenedor">
+        <?php
+        $dni = $_POST['dniins'];
+        $nombre_apellido = $_POST['nombre_apellidoins'];
+        $mail = $_POST['mailins'];
+        include "conecta.php";
+        if (!$conexion) {
+            echo "conexión fallida";
+        } else {
+            echo "Inserción de registro exitoso" . "<br/>" . "<br/>";
+            $ingreso = "INSERT INTO `personas` (`id`, `nombre_apellido`, `mail`, `dni`) VALUES (NULL, '$nombre_apellido', '$mail', '$dni')"; //
+            if ($conexion->query($ingreso)) {
+                echo "Bienvenido <span class='nombre'>" . $nombre_apellido . "</span> a la Expo Solari 2023<br>";
+                echo "Tiene un crédito de <span class='nombre'>\$100000</span> disponible para las compras";
+            } else {
+                echo "Error: " . $ingreso . "<br>" . $conexion->error;
+            }
+        }
+        $conexion->close(); //CIERRA LA CONEXION CON LA BASE
+        ?>
 
+        <br>
+        <br>
+        <div class="buttons">
+            <a href="index.html">Volver al inicio</a>
+        </div>
 
+    </div>
 
 </body>
 </html>
